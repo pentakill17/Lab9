3f876f1d1a12b439e6d90989ce68056a3de03024
reniel <renielbunag08@gmail.com>
2021-04-11 18:25:20 -0400

    git add

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-homepage',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  btnPage()
+  {
+    this.router.navigateByUrl('/about');
+  }
+  btnContact()
+  {
+    this.router.navigateByUrl('/contact');
+  }
 
   ngOnInit(): void {
   }
